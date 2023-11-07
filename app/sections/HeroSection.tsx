856af72d6898c89d5fb2742101d1b7b35bfcc62a
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-11-07 16:41:09 +0200

    fearute: hero desktop

--- app/sections/HeroSection.tsx
+++ app/sections/HeroSection.tsx
@@ -3,16 +3,17 @@ import s from "./HeroSection.module.scss";
 import MainButtonComponent from "@/components/MainButtonComponent";
 import Image from "next/image";
 import HeroTestImage from "@/images/home-hero-test.png"
+import classNames from "classnames";
 
 function HeroSection() {
   return (
-    <div className={s.hero}>
-      <div className={s.hero_text}>
-        <h1>Commodities & Logistics Brokers</h1>
-        <p>Lorem ipsum dolor sit amet consectetur. Commodo leo mauris enim massa. Netus purus fames vel nunc consectetur.</p>
+    <div className={classNames(s.hero, s.container)}>
+      <div className={s.hero__text}>
+        <h1 className={s.hero__heading}>Commodities & Logistics Brokers</h1>
+        <p className={s.hero__paragraph}>Lorem ipsum dolor sit amet consectetur. Commodo leo mauris enim massa. Netus purus fames vel nunc consectetur.</p>
         <MainButtonComponent text="Our Services"/>
       </div>
-      <div className={s.hero_image}>
+      <div className={s.hero__image}>
         <Image src={HeroTestImage} alt="Hero Image"/>
       </div>
     </div>
