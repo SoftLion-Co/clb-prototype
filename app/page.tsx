1344f9523b23f00b6730bb7ea5381e98e8fb3ad1
Paul <115890528+PaulGraur@users.noreply.github.com>
2023-11-07 14:25:15 +0200

    refactor: move header to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import s from "./page.module.scss";
-import HeaderComponent from "@/components/HeaderComponent";
 
 export default function Home() {
   return (
     <div className={s.main}>
-      <HeaderComponent />
     </div>
   );
 }
