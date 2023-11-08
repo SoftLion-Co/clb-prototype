72a4bc6e6ddef609972b40fa6336f5ff7c60629f
Paul <115890528+PaulGraur@users.noreply.github.com>
2023-11-08 08:33:50 +0200

    feature: add service card component and create "our service section"

--- app/sections/main_page/OurServicesSection.tsx
+++ app/sections/main_page/OurServicesSection.tsx
@@ -1,10 +1,13 @@
 import s from "./OurServicesSection.module.scss";
 import MainTitleComponent from "@/components/MainTitleComponent";
+import OurServicesCardComponent from "@/components/main_page/OurServicesCardComponent";
+import classNames from "classnames";
 
 const OurServicesSection = () => {
   return (
-    <section className={s.container}>
+    <section className={classNames(s.container, s.services)}>
       <MainTitleComponent title="Our Services" className={s.services__title} />
+      <OurServicesCardComponent />
     </section>
   );
 };
