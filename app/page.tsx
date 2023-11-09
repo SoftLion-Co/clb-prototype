9f16281e74141f33aed185e346f35253d7735120
Paul <115890528+PaulGraur@users.noreply.github.com>
2023-11-09 15:10:52 +0200

    feature: add page title component and style for it

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import HeroSection from "./sections/HeroSection";
 import OurServicesSection from "./sections/main_page/OurServicesSection";
 import ContactUsSection from "./sections/main_page/ContactUsSection";
 import BlogCardSection from "./sections/main_page/BlogCardSection";
-
+import PageTitleComponent from "@/components/PageTitleComponent";
 export default function Home() {
   return (
     <div className={s.home}>
+      <PageTitleComponent
+        title="about us"
+        text="commodities & Logistics Brokers is your reliable partner in the world of freight transport. We're trusted because we offer time-tested solutions and a professional approach to each client."
+      />
       <HeroSection />
       <OurServicesSection />
       <BlogCardSection />
