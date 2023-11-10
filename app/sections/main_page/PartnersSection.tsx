4164b1501abfd0c1237251690f49101c180be165
Yurii <tahkict.yup4ik@gmail.com>
2023-11-10 14:51:12 +0200

    feature: hotlink problem solved

--- app/sections/main_page/PartnersSection.tsx
+++ app/sections/main_page/PartnersSection.tsx
@@ -1,28 +1,26 @@
-"use client";
-
 import s from "./PartnersSection.module.scss";
 import MainTitleComponent from "@/components/MainTitleComponent";
 import classNames from "classnames";
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
-const reqUrl = "https://softlion.blog/wp-json/wp/v2/partners?acf_format=standard&_fields=id,acf";
+const reqUrl =
+  "https://softlion.blog/wp-json/wp/v2/partners?acf_format=standard&_fields=id,acf";
 
-export type Root = Root2[]
+export type Root = Root2[];
 
 export interface Root2 {
-  id: number
-  acf: Acf
+  id: number;
+  acf: Acf;
 }
 
 export interface Acf {
-  partner_company_logo: string
+  partner_company_logo: string;
 }
 
 const OurServicesSection = async () => {
-  
-    const req = await fetch (reqUrl);
-    const partners: Root2[] = await req.json();
-    console.log(partners)
+  const req = await fetch(reqUrl);
+  const partners: Root2[] = await req.json();
+  console.log(partners);
 
   return (
     <section className={classNames(s.container, s.services)}>
@@ -30,11 +28,13 @@ const OurServicesSection = async () => {
 
       <div className={s.photos}>
         {partners.map((partner, index) => (
-          <img
+          <Image
             key={index}
-            src="https://softlion.blog/wp-content/uploads/2023/07/softlion.png"
+            src={partner.acf.partner_company_logo}
             alt={`Partner Logo ${index + 1}`}
             className={s.photo}
+            width={400}
+            height={200}
           />
         ))}
       </div>
