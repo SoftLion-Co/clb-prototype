37d7a6333e3227298773d3d1c9c7eda9a4fa8b0f
Yurii <tahkict.yup4ik@gmail.com>
2023-11-10 20:27:52 +0200

    Update GetPartnersComponent.tsx

--- components/partners/GetPartnersComponent.tsx
+++ components/partners/GetPartnersComponent.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import s from "@/app/sections/main_page/PartnersSection.module.scss";
 
-const reqUrl = "https://softlion.blog/wp-json/wp/v2/partners?per_page=100&acf_format=standard&_fields=id,acf";
+const reqUrl =
+  "https://softlion.blog/wp-json/wp/v2/partners?per_page=100&acf_format=standard&_fields=id,acf,title";
 
 interface Partners {
+  title: Rendered;
   id: number;
   acf: Acf;
 }
@@ -13,11 +15,15 @@ interface Acf {
   position: number;
 }
 
+interface Rendered {
+  rendered: string;
+}
+
 const GetPartnersComponent = async () => {
   const req = await fetch(reqUrl, { cache: "no-cache" });
   const partners: Partners[] = await req.json();
   partners.sort((a, b) => a.acf.position - b.acf.position);
-  
+
   return (
     <div className={s.partners__wrapper}>
       {partners.map((partner, index) => (
@@ -25,7 +31,7 @@ const GetPartnersComponent = async () => {
           <Image
             key={index}
             src={partner.acf.partner_company_logo}
-            alt={`Partner Logo ${index + 1}`}
+            alt={partner.title.rendered}
             width={200}
             height={100}
             className={s.partners__image}
