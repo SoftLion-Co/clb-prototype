be70c58765a5f1440557e5fa38cc90f7a0174080
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-11-16 11:46:14 +0200

    chore: prettier styling

--- app/sections/commodity_brokerage_page/HeroSection.tsx
+++ app/sections/commodity_brokerage_page/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./HeroSection.module.scss";
 import Image from "next/image";
-import image from "@/images/home-hero-test.png"
+import image from "@/images/home-hero-test.png";
 
 function HeroSection() {
   return (
@@ -12,7 +12,13 @@ function HeroSection() {
         buyers and sellers. We act as your strategic partner, offering in-depth
         market analysis and providing optimal conditions for both parties.
       </div>
-      <Image className={s.hero__image} src={image} alt="Hero image" width={1000} height={1000}/>
+      <Image
+        className={s.hero__image}
+        src={image}
+        alt="Hero image"
+        width={1000}
+        height={1000}
+      />
     </div>
   );
 }
