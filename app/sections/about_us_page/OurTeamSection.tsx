fb6c511b172c6f143fe70de4f0a8facbc809f271
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-01-08 14:30:36 +0200

    refactor: add swipe function and todler button

--- app/sections/about_us_page/OurTeamSection.tsx
+++ app/sections/about_us_page/OurTeamSection.tsx
@@ -1,23 +1,30 @@
 "use client";
-import React, { useCallback, useEffect, useState } from "react";
+import Image from "next/image";
 import classNames from "classnames";
 import s from "./OurTeamSection.module.scss";
-import pagination from "@/components/PaginationComponent.module.scss";
+import React, { useCallback, useEffect, useState } from "react";
 import { Carousel, Embla } from "@mantine/carousel";
-import { Progress } from "@mantine/core";
 
-import ReactPaginate from "react-paginate";
-import Image from "next/image";
 import MainTitleComponent from "@/components/MainTitleComponent";
 import OurTeamCardComponent from "@/components/about_us/OurTeamCardComponent";
+
 import Arrow from "@/images/vectors/arrow.svg";
 import SectionVector from "@/images/vectors/section-vector.svg";
+
 import { useTranslations } from "next-intl";
 import useOurTeamData from "@/hooks/useOurTeamData";
 
 interface TeamMember {
   id: number;
   acf: TeamMemberAcf;
+}
+
+interface OurTeamSectionProps {
+  teamMembers: TeamMember[];
+}
+
+interface ArrowProps {
+  className: string;
 }
 
 interface TeamMemberAcf {
@@ -32,41 +39,16 @@ interface TeamMemberAcf {
   jobtitle_ua: string;
 }
 
-interface OurTeamSectionProps {
-  teamMembers: TeamMember[]; // Assuming TeamMember is defined somewhere
-}
-
 const OurTeamSection: React.FC<OurTeamSectionProps> = ({ teamMembers }) => {
   const t = useTranslations("aboutUs.ourTeam");
-
-  const [currentPage, setCurrentPage] = useState(0);
-  // const itemsPerPage = 4;
   const { team, loading, error } = useOurTeamData();
-
   const reversedTeam = [...team].reverse();
-
-  // const pageCount = Math.ceil(reversedTeam.length / itemsPerPage);
-
-  // const displayedData = reversedTeam.slice(
-  //   currentPage * itemsPerPage,
-  //   (currentPage + 4) * itemsPerPage
-  // );
-
-  const handlePrevPage = () => {
-    setCurrentPage((prev) => (prev > 0 ? prev - 1 : pageCount - 1));
-  };
-
-  const handleNextPage = () => {
-    setCurrentPage((prev) => (prev + 1) % pageCount);
-  };
 
   const [scrollProgress, setScrollProgress] = useState(0);
   const [embla, setEmbla] = useState<Embla | null>(null);
 
   const handleScroll = useCallback(() => {
     if (!embla) return;
-    const progress = Math.max(1, Math.min(1, embla.scrollProgress()));
-    // setScrollProgress(progress * 100);
   }, [embla, setScrollProgress]);
 
   useEffect(() => {
@@ -76,67 +58,64 @@ const OurTeamSection: React.FC<OurTeamSectionProps> = ({ teamMembers }) => {
     }
   }, [embla]);
 
+  const NextArrow: React.FC<ArrowProps> = ({ className }) => {
+    return (
+      <div className={className}>
+        <Image
+          style={{ transform: "rotate(180deg)" }}
+          src={Arrow}
+          alt="Next slide"
+          className={s.arrow}
+          width={28}
+        />
+      </div>
+    );
+  };
+
+  const PrevArrow: React.FC<ArrowProps> = ({ className }) => {
+    return (
+      <div className={className}>
+        <Image
+          src={Arrow}
+          alt="Previous slide"
+          className={s.arrow}
+          width={28}
+        />
+      </div>
+    );
+  };
+
   return (
     <section className={s.box}>
       <div className={classNames(s.background, s.team)}>
         <div className={s.container}>
           <MainTitleComponent className={s.team__title} title="our team" />
 
-          <Carousel
-            dragFree
-            slideSize="25%"
-            slideGap="md"
-            height={400}
-            getEmblaApi={setEmbla}
-            initialSlide={2}
-          >
-            {/* <div className={s.team__cards}> */}
-            {reversedTeam.map((teamMember) => (
-              <Carousel.Slide>
-                <OurTeamCardComponent
-                  key={teamMember.id}
-                  teamMember={teamMember}
-                  className={s.team__cards}
-                />
-              </Carousel.Slide>
-            ))}
-            {/* </div> */}
-          </Carousel>
-
-          <ReactPaginate
-            previousLabel={
-              <div
-                className={pagination.pagination__arrow_previous}
-                // onClick={handlePrevPage}
-              >
-                <Image
-                  className={pagination.pagination__arrow_image}
-                  src={Arrow}
-                  alt="arrow"
-                />
-              </div>
-            }
-            nextLabel={
-              <div
-                className={pagination.pagination__arrow_next}
-                // onClick={handleNextPage}
-              >
-                <Image
-                  className={pagination.pagination__arrow_image}
-                  src={Arrow}
-                  alt="arrow"
-                />
-              </div>
-            }
-            breakClassName={pagination.pagination__break}
-            onPageChange={(selected) => setCurrentPage(selected.selected)}
-            containerClassName={pagination.pagination}
-            pageClassName={pagination.pagination__pages}
-            activeClassName={pagination.pagination__active}
-            pageLinkClassName={pagination.hidden}
-            forcePage={currentPage}
-            pageCount={0}
-          />
+          <div className={s.team__carousel}>
+            <Carousel
+              classNames={{
+                control: s.team__control,
+                controls: s.team__controls,
+              }}
+              height={400}
+              slideSize="25%"
+              slideGap="md"
+              loop
+              align="start"
+              slidesToScroll={1}
+              previousControlIcon={<NextArrow className={s.arrow} />}
+              nextControlIcon={<PrevArrow className={s.arrow} />}
+            >
+              {reversedTeam.map((teamMember) => (
+                <Carousel.Slide key={teamMember.id}>
+                  <OurTeamCardComponent
+                    teamMember={teamMember}
+                    className={s.team__cards}
+                  />
+                </Carousel.Slide>
+              ))}
+            </Carousel>
+          </div>
         </div>
         <Image className={s.team__vector} src={SectionVector} alt="vector" />
       </div>
