96ebf9b466553d64fbd7abadcc44c75b898cdd25
Yurii <tahkict.yup4ik@gmail.com>
2024-01-13 13:42:04 +0200

    feature: hero rest api

--- app/sections/main_page/HeroSection.tsx
+++ app/sections/main_page/HeroSection.tsx
@@ -1,65 +1,24 @@
-"use client";
-import React, { useEffect, useState } from "react";
+"use client"
+import React from "react";
 import s from "./HeroSection.module.scss";
 import MainButtonComponent from "@/components/MainButtonComponent";
-import Image from "next/image";
-import HeroImage from "@/images/hero_video/photo1080.webp";
 import { useTranslations } from "next-intl";
-import { useNetwork, useOs } from "@mantine/hooks";
-import useHeroVideo from "@/hooks/useHeroVideo";
+import GetHeroVideoComponent from "@/components/main_page/GetHeroVideoComponent";
+import GetHeroImageComponent from "@/components/main_page/GetHeroImageComponent";
+import { useNetwork } from '@mantine/hooks'; // Import the useNetwork hook
 
 function HeroSection() {
-  const [shouldRenderPhoto, setShouldRenderPhoto] = useState(true);
-  const { online, effectiveType } = useNetwork();
-  const os = useOs();
-  const heroVideo = useHeroVideo();
-
   const t = useTranslations("homePage");
   const t1 = useTranslations("components");
-
-  useEffect(() => {
-    // Detect if the user agent is Safari
-    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-    // Determine whether to render the photo or video based on network speed, online status, OS, and Safari browser
-    const shouldRender =
-      !heroVideo.videoWEBM ||
-      !heroVideo.videoMP4 ||
-      !heroVideo.heroPhoto ||
-      effectiveType === "slow-2g" ||
-      effectiveType === "2g" ||
-      (os === "macos" && isSafari);
-
-    setShouldRenderPhoto(shouldRender);
-  }, [online, effectiveType, os, heroVideo.heroPhoto, heroVideo.videoWEBM, heroVideo.videoMP4]);
-  console.log("heroVideo.heroPhoto:", heroVideo.heroPhoto);
+  const { effectiveType } = useNetwork(); // Get the user's connection type
 
   return (
     <section className={s.box}>
       <div className={s.hero}>
-        {shouldRenderPhoto ? (
-          <Image
-            className={s.hero__video}
-            src={heroVideo.heroPhoto}
-            alt="Hero Image"
-            width={2000}
-            height={2000}
-            loading="lazy"
-            fetchPriority="high"
-          />
-        ) : (
-          <video
-            className={s.hero__video}
-            width="100%"
-            height="100%"
-            autoPlay
-            playsInline
-            loop
-            muted
-          >
-            <source src={heroVideo.videoWEBM} type="video/webm" />
-            <source src={heroVideo.videoMP4} type="video/mp4" />
-          </video>
+      {effectiveType === "slow-2g" || effectiveType === "2g" || effectiveType === "3g" ? ( // Render photo if the connection is bad
+          <GetHeroImageComponent />
+        ) : ( // Render video if the connection is good
+          <GetHeroVideoComponent />
         )}
         <div className={s.hero__content}>
           <h1 className={s.hero__title}>{t("hero")}</h1>
---
+++ components/main_page/GetHeroVideoComponent.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import s from "@/app/sections/main_page/HeroSection.module.scss";
+import GetHeroImageComponent from "./GetHeroImageComponent";
+
+interface VideoData {
+  acf: {
+    hero_video_webm: string;
+    hero_video_mp4: string;
+  };
+}
+
+const videoReqUrl =
+  "https://softlion.blog/wp-json/wp/v2/videos/500?acf_format=standard&_fields=acf";
+
+const GetHeroVideoComponent = () => {
+  const [video, setVideo] = useState<VideoData | null>(null);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(videoReqUrl)
+      .then((response) => response.json())
+      .then((data: VideoData) => setVideo(data))
+      .catch((error) => console.error("Error fetching video:", error));
+  }, []);
+
+  useEffect(() => {
+    if (video) {
+      setIsVideoLoaded(true);
+    }
+  }, [video]);
+
+  if (!video) {
+    // Video data is still being fetched
+    return <div className={s.hero__video}></div>; // or display a loading spinner/message
+  }
+
+  return (
+    
+      <video
+        className={s.hero__video}
+        width="100%"
+        height="100%"
+        autoPlay
+        playsInline
+        loop
+        muted
+      >
+        <source src={video.acf.hero_video_webm} type="video/webm" />
+        <source src={video.acf.hero_video_mp4} type="video/mp4" />
+      </video>
+    
+  );
+};
+
+export default GetHeroVideoComponent;
