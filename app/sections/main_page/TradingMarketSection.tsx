87ae09d185c5424800cdf7d75e751e40a2b56e1b
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-01-24 10:51:46 +0200

    /

--- app/sections/main_page/TradingMarketSection.tsx
+++ app/sections/main_page/TradingMarketSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import s from "./TradingMarketSection.module.scss";
 import MainTitleComponent from "@/components/MainTitleComponent";
 import MapBoxComponent from "@/components/map_component/MapBoxComponent";
@@ -9,6 +9,12 @@ import countriesData from "@/components/map_component/countriesData";
 const TradingMarketSection = () => {
   const t = useTranslations("homePage");
 
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const handleCountrySelect = (countryData: any) => {
+    setSelectedCountry(countryData);
+  };
+
   return (
     <section className={s.box}>
       <div className={s.background}>
@@ -16,24 +22,33 @@ const TradingMarketSection = () => {
           <MainTitleComponent title={t("tradingMarkets")} color="black" />
           <div className={s.map}>
             <div className={s.map__container}>
-              <MapBoxComponent />
+              <MapBoxComponent onCountrySelect={handleCountrySelect} />
             </div>
 
             <div className={s.map__content}>
-              <h2 className={s.map__title}>{/* here country name */}</h2>
-
-              <ul className={s.map__list}>
-                <p className={s.map__subtitle}>Cultures:</p>
-                <li className={s.map__item}>{/* here agriculturalCrops */}</li>
-              </ul>
-
-              <div className={s.map__delivery}>
-                <p className={s.map__subtitle}>Delivery options:</p>
-
-                <ul className={s.map__options}>
-                  <li className={s.map__items}>{/* here deliveryOptions */}</li>
-                </ul>
-              </div>
+              {selectedCountry && (
+                <>
+                  <h2 className={s.map__title}>{selectedCountry.country}</h2>
+                  <ul className={s.map__list}>
+                    <p className={s.map__subtitle}>Cultures:</p>
+                    {selectedCountry.agriculturalCrops.map((crop, index) => (
+                      <li key={index} className={s.map__item}>
+                        {crop}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className={s.map__delivery}>
+                    <p className={s.map__subtitle}>Delivery options:</p>
+                    <ul className={s.map__options}>
+                      {selectedCountry.deliveryOptions.map((option, index) => (
+                        <li key={index} className={s.map__items}>
+                          {option}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
