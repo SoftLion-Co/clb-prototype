e531c41dfcc43434e507fa9864509254bbad9803
Yurii <tahkict.yup4ik@gmail.com>
2024-02-09 14:31:10 +0200

    refactor: careers cards restAPI

--- app/sections/careers_page/CardsSection.tsx
+++ app/sections/careers_page/CardsSection.tsx
@@ -1,16 +1,42 @@
 import s from "./CardsSection.module.scss";
 import Image from "next/image";
-import PageTitleComponent from "@/components/PageTitleComponent";
 import ImageAndTextCardsComponent from "@/components/ImageAndTextCardsComponent";
 import image1 from "@/images/careers/1.png";
 import image2 from "@/images/careers/2.png";
 import BrandElement from "@/images/vectors/brand-element-5.svg";
 import classNames from "classnames";
-import { useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 import GetHeroComponent from "@/hooks/GetHeroComponent";
 
-const CardsSection = () => {
-  const t = useTranslations("careers");
+export interface CareersCards {
+  acf: Acf;
+}
+export interface Acf {
+  image1: string;
+  image2: string;
+  card1_en: string;
+  card2_en: string;
+  card1_es: string;
+  card2_es: string;
+  card1_de: string;
+  card2_de: string;
+  card1_ua: string;
+  card2_ua: string;
+}
+
+const CardsSection = async() => {
+  const reqUrl = `https://softlion.blog/wp-json/wp/v2/careers-cards?acf_format=standard&_fields=acf`
+
+  const req = await fetch(reqUrl);
+  const storyData: CareersCards[] = await req.json();
+  const locale = useLocale();
+
+  const { acf } = storyData[0];
+
+  const card1Text = (acf as any)[`card1_${locale}`];
+  const card1Image = acf.image1;
+  const card2Text = (acf as any)[`card2_${locale}`];
+  const card2Image = acf.image2;
 
   return (
     <section className={classNames(s.box, s.cards)}>
@@ -20,15 +46,15 @@ const CardsSection = () => {
         </div>
         <div className={classNames(s.container, s.cards__container)}>
           <ImageAndTextCardsComponent
-            text={t("cardsBigText")}
-            image={image1.src}
+            text={card1Text}
+            image={card1Image}
             alt={"Image"}
             mobileTextCenter
           />
           <ImageAndTextCardsComponent
             rotate
-            text={t("cardsSmallText")}
-            image={image2.src}
+            text={card2Text}
+            image={card2Image}
             alt={"Image"}
             color="white"
             border
