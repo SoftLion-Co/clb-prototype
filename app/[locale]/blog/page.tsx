78af64cb059569d0bda905fb9f9029fa2baf1a12
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-02-19 16:15:07 +0200

    refactor: remove invalid generate metadata

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
 import BlogSection from "@/app/sections/blog_page/BlogSection";
 import ContactUsSection from "@/app/sections/main_page/ContactUsSection";
@@ -11,25 +11,11 @@ export async function generateMetadata({}) {
   return { title: "Blog" };
 }
 
-export default function Blog() {
-  const [metadata, setMetadata] = useState({ title: "" });
+export default function Blog({}) {
   const { blogs, loading, error } = useBlogsData();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await generateMetadata({});
-      setMetadata(data);
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <React.Fragment>
-      <Helmet>
-        <title>{metadata.title}</title>
-      </Helmet>
-
       <div>
         {blogs.length === 0 && <BlogSkeleton />}
         {error && <p>Error: {error}</p>}
